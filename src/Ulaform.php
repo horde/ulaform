1d95e704dfc25875d1726feaa156a2c857dd69fd
Ralf Lang <ralf.lang@ralf-lang.de>
2026-05-19 19:51:24 +0200

    chore: Require PHP 8

---
+++ src/Ulaform.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright 2013-2026 The Horde Project (http://www.horde.org/)
+ *
+ * See the enclosed file LICENSE for license information (GPL-2.0). If you
+ * did not receive this file, see http://www.horde.org/licenses/gpl.php.
+ *
+ * @author   Chuck Hagenbuch <chuck@horde.org>
+ * @category Horde
+ * @license  http://www.horde.org/licenses/gpl.php GPL-2.0
+ * @package  Ulaform
+ */
+
+declare(strict_types=1);
+
+namespace Horde\Ulaform;
+
+/**
+ * Ulaform application class.
+ *
+ * @author    Chuck Hagenbuch <chuck@horde.org>
+ * @category  Horde
+ * @copyright 2013-2026 The Horde Project
+ * @license   http://www.horde.org/licenses/gpl.php GPL-2.0
+ * @package   Ulaform
+ */
+class Ulaform
+{
+}
